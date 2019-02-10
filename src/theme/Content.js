2b9eb473dbbcdd11105770d4295ccd4fb0f45ae3
Charlie Kriewall <charlie@kriewall.com>
2019-02-10 14:08:17 -0800

    add React Router support

--- src/theme/Content.js
+++ src/theme/Content.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Tooltip from '@material-ui/core/Tooltip';
-import IconButton from '@material-ui/core/IconButton';
-import { withStyles } from '@material-ui/core/styles';
-import SearchIcon from '@material-ui/icons/Search';
-import RefreshIcon from '@material-ui/icons/Refresh';
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+import { withStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import RefreshIcon from "@material-ui/icons/Refresh";
 
 const styles = theme => ({
   paper: {
     maxWidth: 936,
-    margin: 'auto',
-    overflow: 'hidden',
+    margin: "auto",
+    overflow: "hidden"
   },
   searchBar: {
-    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+    borderBottom: "1px solid rgba(0, 0, 0, 0.12)"
   },
   searchInput: {
-    fontSize: theme.typography.fontSize,
+    fontSize: theme.typography.fontSize
   },
   block: {
-    display: 'block',
+    display: "block"
   },
   addUser: {
-    marginRight: theme.spacing.unit,
+    marginRight: theme.spacing.unit
   },
   contentWrapper: {
-    margin: '40px 16px',
-  },
+    margin: "40px 16px"
+  }
 });
 
 function Content(props) {
@@ -41,7 +41,12 @@ function Content(props) {
 
   return (
     <Paper className={classes.paper}>
-      <AppBar className={classes.searchBar} position="static" color="default" elevation={0}>
+      <AppBar
+        className={classes.searchBar}
+        position="static"
+        color="default"
+        elevation={0}
+      >
         <Toolbar>
           <Grid container spacing={16} alignItems="center">
             <Grid item>
@@ -53,12 +58,16 @@ function Content(props) {
                 placeholder="Search by email address, phone number, or user UID"
                 InputProps={{
                   disableUnderline: true,
-                  className: classes.searchInput,
+                  className: classes.searchInput
                 }}
               />
             </Grid>
             <Grid item>
-              <Button variant="contained" color="primary" className={classes.addUser}>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.addUser}
+              >
                 Add user
               </Button>
               <Tooltip title="Reload">
@@ -80,7 +89,7 @@ function Content(props) {
 }
 
 Content.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Content);
--- src/theme/Paperbase.js
+++ src/theme/Paperbase.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { MuiThemeProvider, createMuiTheme, withStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Hidden from '@material-ui/core/Hidden';
-import Navigator from './Navigator';
-import Content from './Content';
-import Header from './Header';
+import React from "react";
+import PropTypes from "prop-types";
+import { Route } from "react-router-dom";
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  withStyles
+} from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Hidden from "@material-ui/core/Hidden";
+import Navigator from "./Navigator";
+import Content from "./Content";
+import Header from "./Header";
+
+import Component1 from "../components/Component1";
+import Component2 from "../components/Component2";
 
 let theme = createMuiTheme({
   typography: {
@@ -13,19 +21,19 @@ let theme = createMuiTheme({
     h5: {
       fontWeight: 500,
       fontSize: 26,
-      letterSpacing: 0.5,
-    },
+      letterSpacing: 0.5
+    }
   },
   palette: {
     primary: {
-      light: '#63ccff',
-      main: '#009be5',
-      dark: '#006db3',
-    },
+      light: "#63ccff",
+      main: "#009be5",
+      dark: "#006db3"
+    }
   },
   shape: {
-    borderRadius: 8,
-  },
+    borderRadius: 8
+  }
 });
 
 theme = {
@@ -33,124 +41,124 @@ theme = {
   overrides: {
     MuiDrawer: {
       paper: {
-        backgroundColor: '#18202c',
-      },
+        backgroundColor: "#18202c"
+      }
     },
     MuiButton: {
       label: {
-        textTransform: 'initial',
+        textTransform: "initial"
       },
       contained: {
-        boxShadow: 'none',
-        '&:active': {
-          boxShadow: 'none',
-        },
-      },
+        boxShadow: "none",
+        "&:active": {
+          boxShadow: "none"
+        }
+      }
     },
     MuiTabs: {
       root: {
-        marginLeft: theme.spacing.unit,
+        marginLeft: theme.spacing.unit
       },
       indicator: {
         height: 3,
         borderTopLeftRadius: 3,
         borderTopRightRadius: 3,
-        backgroundColor: theme.palette.common.white,
-      },
+        backgroundColor: theme.palette.common.white
+      }
     },
     MuiTab: {
       root: {
-        textTransform: 'initial',
-        margin: '0 16px',
+        textTransform: "initial",
+        margin: "0 16px",
         minWidth: 0,
-        [theme.breakpoints.up('md')]: {
-          minWidth: 0,
-        },
+        [theme.breakpoints.up("md")]: {
+          minWidth: 0
+        }
       },
       labelContainer: {
         padding: 0,
-        [theme.breakpoints.up('md')]: {
-          padding: 0,
-        },
-      },
+        [theme.breakpoints.up("md")]: {
+          padding: 0
+        }
+      }
     },
     MuiIconButton: {
       root: {
-        padding: theme.spacing.unit,
-      },
+        padding: theme.spacing.unit
+      }
     },
     MuiTooltip: {
       tooltip: {
-        borderRadius: 4,
-      },
+        borderRadius: 4
+      }
     },
     MuiDivider: {
       root: {
-        backgroundColor: '#404854',
-      },
+        backgroundColor: "#404854"
+      }
     },
     MuiListItemText: {
       primary: {
-        fontWeight: theme.typography.fontWeightMedium,
-      },
+        fontWeight: theme.typography.fontWeightMedium
+      }
     },
     MuiListItemIcon: {
       root: {
-        color: 'inherit',
+        color: "inherit",
         marginRight: 0,
-        '& svg': {
-          fontSize: 20,
-        },
-      },
+        "& svg": {
+          fontSize: 20
+        }
+      }
     },
     MuiAvatar: {
       root: {
         width: 32,
-        height: 32,
-      },
-    },
+        height: 32
+      }
+    }
   },
   props: {
     MuiTab: {
-      disableRipple: true,
-    },
+      disableRipple: true
+    }
   },
   mixins: {
     ...theme.mixins,
     toolbar: {
-      minHeight: 48,
-    },
-  },
+      minHeight: 48
+    }
+  }
 };
 
 const drawerWidth = 256;
 
 const styles = {
   root: {
-    display: 'flex',
-    minHeight: '100vh',
+    display: "flex",
+    minHeight: "100vh"
   },
   drawer: {
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up("sm")]: {
       width: drawerWidth,
-      flexShrink: 0,
-    },
+      flexShrink: 0
+    }
   },
   appContent: {
     flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
+    display: "flex",
+    flexDirection: "column"
   },
   mainContent: {
     flex: 1,
-    padding: '48px 36px 0',
-    background: '#eaeff1',
-  },
+    padding: "48px 36px 0",
+    background: "#eaeff1"
+  }
 };
 
 class Paperbase extends React.Component {
   state = {
-    mobileOpen: false,
+    mobileOpen: false
   };
 
   handleDrawerToggle = () => {
@@ -179,8 +187,12 @@ class Paperbase extends React.Component {
           </nav>
           <div className={classes.appContent}>
             <Header onDrawerToggle={this.handleDrawerToggle} />
+
+            {/* React Router routes go here } */}
             <main className={classes.mainContent}>
               <Content />
+              <Route path="/component1" component={Component1} />
+              <Route path="/component2" component={Component2} />
             </main>
           </div>
         </div>
@@ -190,7 +202,7 @@ class Paperbase extends React.Component {
 }
 
 Paperbase.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Paperbase);
